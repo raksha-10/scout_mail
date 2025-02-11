3eff928e50ec8179357a1a81fd876ccf83f9fd7c
raksha bhalavi <raksha.b@goldeneagle.ai>
2025-02-11 19:23:48 +0530

    update user profile

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::UsersController < ApplicationController
-    before_action :authenticate_user!
-    skip_before_action :authenticate_user!, only: [:accept_invitation]
+  before_action :authenticate_user!, except: [:accept_invitation]
 
     def invite_user
       @organisation = Organisation.find(params[:organisation_id])
@@ -30,7 +29,15 @@ class Api::V1::UsersController < ApplicationController
         render json: { error: 'Failed to associate the user with the organization.' }, status: :unprocessable_entity
       end
     end
-    
+
+    def update
+      if current_user.update(user_params)
+        render json: { message: "Profile updated successfully", user: current_user }, status: :ok
+      else
+        render json: { errors: current_user.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+     
     def accept_invitation
       token = params[:invitation_token]
       user = User.find_by_invitation_token(token, true)
@@ -41,6 +48,12 @@ class Api::V1::UsersController < ApplicationController
       else
         render json: { error: "Invalid or expired invitation token" }, status: :unprocessable_entity
       end
+    end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:name, :password, :password_confirmation)
     end
    
   end
